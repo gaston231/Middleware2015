1550f254d76dd4c1698cb9a2ecdf5eeb4255d9b6
cvelez <alberto12>
2015-09-26 14:42:33 -0300

    sd

--- Obligatorio1/Central_Midd/src/main/java/org/springframework/Request.java
+++ Obligatorio1/Central_Midd/src/main/java/org/springframework/Request.java
@@ -9,14 +9,22 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.util.ArrayList;
+
+import javax.xml.XMLConstants;
 import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
 import org.apache.log4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -29,6 +37,7 @@ import org.springframework.messaging.core.DestinationResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 
 
@@ -40,21 +49,34 @@ public class Request {
 	public String obtenerRequest(String requestXML) throws Exception {
 
 		
+		try {
+			
+			String xsdPath = null; // path donde esta el archivo xsd
+        	Source source = new StreamSource(new StringReader(requestXML)); 
+            SchemaFactory factory = 
+                    SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            
+			Schema schema = factory.newSchema(new File(xsdPath));
+            Validator validator = schema.newValidator();
+            validator.validate(source);
+        } catch (IOException e) {
+            
+            throw new Exception(e.getMessage());
+        }
+		catch (SAXException e) 
+		{
+			throw new Exception(e.getMessage());
+			
+		}
+		
+		
 		System.out.println(requestXML);
 		
 		return requestXML;
 		
 		
 		
-		
-		
-//		issueResponseFor("ss");
-//				ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
-//				"/META-INF/integration/context.xml");
-//		RequestGateway requestGateway = context.getBean("requestGateway", RequestGateway.class);
-//		String reply = requestGateway.ResponseUno("ddddddd");
-//		System.out.println(reply);
-//		logger.info("\n\n++++++++++++ Replied with: " + reply + " ++++++++++++\n");
+	
 		
 	}
 }
