5bf4a79618db40820437e0be20c7eba38ac5fcbb
ggimenez91 <a.gaston.gimenez@gmail.com>
2015-09-26 18:19:57 -0300

    Cambio formato que devuelven ws partners

--- Obligatorio1/EmpresasPartner/src/middleware/EmpresaVentaEntradas.java
+++ Obligatorio1/EmpresasPartner/src/middleware/EmpresaVentaEntradas.java
@@ -11,24 +11,33 @@ import com.sun.xml.ws.developer.SchemaValidation;
 @WebService
 @SchemaValidation
 public class EmpresaVentaEntradas {
-	static Integer cantidadEntradasDisponibles = 20;
+	static Integer cantidadEntradasDisponibles = 200;
 	static Long idCobro = (long) 0;
 	static Integer idEntradas = 100000;
 	
 	@WebMethod
 	public ResultadoVenta ventaEntrada(Short cantidadEntradas, String codigoMoneda, double precioUnitario, Date fechaHora) throws CantidadNoDisponible{
-		ResultadoVenta result = new ResultadoVenta();
-		result.codigosDeEntrada = new ArrayList<String>();
+		ResultadoVenta resultado = new ResultadoVenta();
+		resultado.codigosDeEntrada = new ArrayList<String>();
+		Integer idCobroError = -1;
+		
+		// Verifico disponibilidad de entradas
 		if (cantidadEntradas > cantidadEntradasDisponibles){
-            throw new CantidadNoDisponible("Cantidad no disponible. Máximo disponible: " + cantidadEntradasDisponibles);
+            //throw new CantidadNoDisponible("Cantidad no disponible. Máximo disponible: " + cantidadEntradasDisponibles);
+			resultado.codigoResultado = -1;
+			resultado.mensajeResultado = "Cantidad no disponible. Máximo disponible: " + cantidadEntradasDisponibles;
+			resultado.idCobro = idCobroError.longValue();
+		} else {
+			for (int i = 1; i <= cantidadEntradas; i++){
+				resultado.codigosDeEntrada.add(idEntradas.toString());
+				idEntradas++;
+			}
+			idCobro++;
+			resultado.idCobro = idCobro;
+			resultado.codigoResultado = 0;
+			resultado.mensajeResultado = "OK";
 		}
-		for (int i = 1; i <= cantidadEntradas; i++){
-			result.codigosDeEntrada.add(idEntradas.toString());
-			idEntradas++;
-		}
-		idCobro++;
-		result.idCobro = idCobro;
-		return result;
+		return resultado;
     }
 
 }
